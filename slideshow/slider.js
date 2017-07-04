f093d640da120b5e1a39efc7e955371244d6c818
TrungNguyen <nguyen0611@gmail.com>
2017-07-04 17:34:25 +0700

    update slider

--- slideshow/slider.js
+++ slideshow/slider.js
@@ -25,9 +25,6 @@ function showSlide(n) {
 	var i;
 	var x = document.getElementsByClassName("mySlides");
 	var y = document.getElementsByClassName("description");
-	var z = document.getElementsByClassName("mySlides").classList;
-	// z[3].remove("animate-fading");
-	// z[3].add("animate-fading-left");
 	if (n > x.length) {
 		currentIndex = 1
 	}
@@ -42,18 +39,15 @@ function showSlide(n) {
 //auto transistion slide function
 function tranSlide() {
 	var i;
-	    var x = document.getElementsByClassName("mySlides");
-	    var y = document.getElementsByClassName("description");
-	    for (i = 0; i < x.length; i++) {
-	       x[i].style.display = "none";  
-	       y[i].style.display = "none";
-	    }
-	    currentIndex++;
-	    if (currentIndex > x.length) {currentIndex = 1}  {
-	    	y[currentIndex-1].style.display = "block";
-	    	x[currentIndex-1].style.display = "block";
-	    }  
-	      
-
-	    myTimeout = setTimeout(tranSlide, 4000);    
+	var x = document.getElementsByClassName("mySlides");
+	var y = document.getElementsByClassName("description");
+	for (i = 0; i < x.length; i++) {
+	    x[i].style.display = "none";  
+	    y[i].style.display = "none";
+	}
+	if (currentIndex > x.length) {currentIndex = 1}
+	y[currentIndex-1].style.display = "block";
+	x[currentIndex-1].style.display = "block";
+	currentIndex++;
+	myTimeout = setTimeout(tranSlide, 4000);    
 }
