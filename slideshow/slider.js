149804475e498428b0c4d77cd63f917682e20d74
TrungNguyen <nguyen0611@gmail.com>
2017-07-04 17:13:40 +0700

    update slider

--- slideshow/slider.js
+++ slideshow/slider.js
@@ -1,21 +1,58 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-//auto trans
-setInterval(function(){showDivs(slideIndex += 1);}, 4*1000);
-function plusDivs(n) {
-	showDivs(slideIndex += n);
+var currentIndex = 1;
+var myTimeout;
+var dots = document.getElementsByClassName("dot");
+//display img[0]
+showSlide(currentIndex);
+// Auto transistion slide
+// tranSlide();
+
+function plusSlide(n) {
+	showSlide(currentIndex += n);
+	// clearTimeout(myTimeout);
+	// myTimeout = setTimeout(tranSlide, 4000);
 }
-function currentDiv(n) {
-	slideIndex = n;
-	showDivs(slideIndex);
+function currentSlide(n) {
+	currentIndex = n;
+	showSlide(currentIndex);
+	// clearTimeout(myTimeout);
+	// myTimeout = setTimeout(tranSlide, 4000);
 }
-function showDivs(n) {
+/*
+*
+*/
+function showSlide(n) {
 	var i;
 	var x = document.getElementsByClassName("mySlides");
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length}
+	var y = document.getElementsByClassName("description");
+	var z = document.getElementsByClassName("mySlides").classList;
+	// z[3].remove("animate-fading");
+	// z[3].add("animate-fading-left");
+	if (n > x.length) {
+		currentIndex = 1
+	}
+	if (n < 1) {currentIndex = x.length}
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
+		y[i].style.display = "none";
 	}
-	x[slideIndex-1].style.display = "block";
+	x[currentIndex-1].style.display = "block";
+	y[currentIndex-1].style.display = "block"
+}
+//auto transistion slide function
+function tranSlide() {
+	var i;
+	    var x = document.getElementsByClassName("mySlides");
+	    var y = document.getElementsByClassName("description");
+	    for (i = 0; i < x.length; i++) {
+	       x[i].style.display = "none";  
+	       y[i].style.display = "none";
+	    }
+	    currentIndex++;
+	    if (currentIndex > x.length) {currentIndex = 1}  {
+	    	y[currentIndex-1].style.display = "block";
+	    	x[currentIndex-1].style.display = "block";
+	    }  
+	      
+
+	    myTimeout = setTimeout(tranSlide, 4000);    
 }
