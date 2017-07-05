11b0d70ac427e37774400600f98cd07964e4d873
TrungNguyen <nguyen0611@gmail.com>
2017-07-05 14:45:28 +0700

    update slider

--- slideshow/js/slider.js
+++ slideshow/js/slider.js
@@ -1,5 +1,5 @@
 //start in first slide index of image equal 0
-var currentIndex = 1;
+var CURRENT_INDEX = 1;
 //list of  images/descrption image/image dots
 var images = document.getElementsByClassName("mySlides");
 var descriptions = document.getElementsByClassName("description");
@@ -7,16 +7,16 @@ var dots = document.getElementsByClassName("dot");
 //time to auto transistion by second
 var TIME_AUTO_SEC = 4;
 //ID of timeout
-var myTimeOut;
+var ID_TIME_OUT;
 
 /*Load function when startup JS
-* @para currentIndex
-* showSlide func will run with currentIndex
+* @para CURRENT_INDEX
+* showSlide func will run with CURRENT_INDEXCURRENT_INDEX
 * tranSlide func will auto transition slide start in slide 1
 */
-showSlide(currentIndex);
+showSlide(CURRENT_INDEX);
 //auto transistion slide
-// myTimeout = setInterval(function(){showSlide(currentIndex += 1);}, TIME_AUTO_SEC*1000);
+// myTimeout = setInterval(function(){showSlide(CURRENT_INDEX += 1);}, TIME_AUTO_SEC*1000);
 
 /* Below func is move to slide next/prev
 *@para n
@@ -25,14 +25,14 @@ showSlide(currentIndex);
 * n=-1 to prev slide
 */
 function plusSlide(n) {
-	showSlide(currentIndex += n);
+	showSlide(CURRENT_INDEX += n);
 }
 /*move to slide that have index equal index of active dot
 *    @para n
 * n is number of order dot
 */
 function currentSlide(n) {
-	showSlide(currentIndex = n);
+	showSlide(CURRENT_INDEX = n);
 }
 /*main function to show slide
 * @para n
@@ -41,22 +41,22 @@ function currentSlide(n) {
 function showSlide(n) {
 	var i;
 	//case n more than size of images_list will show first image
-	if (n > images.length) {currentIndex = 1}
+	if (n > images.length) {CURRENT_INDEX = 1}
 	//case n less than size of images_list will show last image
-	if (n < 1) {currentIndex = images.length}
+	if (n < 1) {CURRENT_INDEX = images.length}
 	//initial to none-display images
 	for (i = 0; i < images.length; i++) {
 		images[i].style.display = "none";
 		descriptions[i].style.display = "none";
 		dots[i].style.color = "#000";
 	}
-	//set to display image have index=currentIndex
-	images[currentIndex-1].style.display = "block";
-	descriptions[currentIndex-1].style.display = "block"
-	dots[currentIndex-1].style.color = "#fff";
+	//set to display image have index=CURRENT_INDEX
+	images[CURRENT_INDEX-1].style.display = "block";
+	descriptions[CURRENT_INDEX-1].style.display = "block"
+	dots[CURRENT_INDEX-1].style.color = "#fff";
 	/*process to auto transistion to next slide
 	* reset time out after active prev/next button or image dots
 	*/
-	clearInterval(myTimeOut);
-	myTimeOut = setInterval(function(){showSlide(currentIndex += 1);}, TIME_AUTO_SEC*1000);
+	clearInterval(ID_TIME_OUT);
+	ID_TIME_OUT = setInterval(function(){showSlide(CURRENT_INDEX += 1);}, TIME_AUTO_SEC*1000);
 }
